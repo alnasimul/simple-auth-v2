a82c2ebe50aa7bcd116825a4a5eec264dacd4c8d
Al Nasimul Haque <alnasimul@gmail.com>
2021-02-16 23:46:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
 import './App.css';
+import firebaseConfig from './simple-auth-v2-config';
 
+firebase.initializeApp(firebaseConfig);
 function App() {
+
+  const handelBlur = (e) => {
+    console.log(e.target.name,e.target.value)
+    if(e.target.name === "email"){
+        const re = /\S+@\S+\.\S+/;
+        let isEmailValid = re.test(e.target.value);
+        console.log(isEmailValid);
+    }else if(e.target.name === "password"){
+        const isPasswordValid = e.target.value.length > 6 ;
+        const re = /\d{1}/;
+        const passwordHasNumber = re.test(e.target.value);
+        console.log(isPasswordValid && passwordHasNumber);
+    }
+  } 
+
+  const handelSubmit = () => {
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Our own Authentication</h1>
+      <form onSubmit={handelSubmit}>
+        <input type="text" name="email" placeholder="Your Email Address" required onBlur={handelBlur} />
+        <br />
+        <input type="password" name="password" id="" placeholder="Your Password" required onBlur={handelBlur} />
+        <br />
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
---
+++ src/simple-auth-v2-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIDEbklaCz-NUZah4lC2VU1CiEe8j7AJI",
+    authDomain: "simple-auth-01.firebaseapp.com",
+    projectId: "simple-auth-01",
+    storageBucket: "simple-auth-01.appspot.com",
+    messagingSenderId: "649259063000",
+    appId: "1:649259063000:web:9fea8bb3f5133f22c5a9ad"
+  };
+
+export default firebaseConfig;
